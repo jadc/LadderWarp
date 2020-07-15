fa38a180699cf39c0c3c1d318e8e747469b5e804
jadc <chehimj@gmail.com>
2020-07-15 16:20:40 -0600

    exploits patched

--- src/main/java/red/jad/ladderwarp/WarpClimbableMixin.java
+++ src/main/java/red/jad/ladderwarp/WarpClimbableMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,21 +20,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({LadderBlock.class, VineBlock.class, TwistingVinesPlantBlock.class, WeepingVinesPlantBlock.class, TwistingVinesBlock.class, WeepingVinesBlock.class})
 public class WarpClimbableMixin {
-
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(!world.isClient()){
+            if(player.getMainHandStack().isEmpty() && !player.isSneaking()){
+                int up = 0, down = 0;
+                while(world.getBlockState(pos.add(0, up + 1, 0)).getBlock().isIn(BlockTags.CLIMBABLE)) up++;
+                while(world.getBlockState(pos.add(0, down - 1, 0)).getBlock().isIn(BlockTags.CLIMBABLE)) down--;
+                if(up + down != 0 && Math.abs(up + down) != 1){
+                    up--; // to not suffocate
+                    double newY = pos.getY() + (up > Math.abs(down) ? up : down);
+                    if(world.getBlockState(player.getBlockPos()).getBlock().isIn(BlockTags.CLIMBABLE)){
+                        if(pos.getX() != Math.floor(player.getX())) return ActionResult.PASS;
+                        if(pos.getZ() != Math.floor(player.getZ())) return ActionResult.PASS;
+                    }
 
-        if(!player.isSneaking()){
-            int up = 0, down = 0;
-            while(world.getBlockState(pos.add(0, up + 1, 0)).getBlock().isIn(BlockTags.CLIMBABLE)) up++;
-            while(world.getBlockState(pos.add(0, down - 1, 0)).getBlock().isIn(BlockTags.CLIMBABLE)) down--;
-            if(world.getBlockState(pos.add(0, up + 1, 0)).getCollisionShape(world, pos.add(0, up + 1, 0)) != null) up--;
-            if(up > 0 || down < 0){
-                int change = up >= Math.abs(down) ? up : down;
-                player.teleport(pos.getX() + 0.5, pos.getY() + change, pos.getZ() + 0.5);
-                world.playSound(null, player.getBlockPos(), world.getBlockState(pos).getBlock().getSoundGroup(world.getBlockState(pos)).getStepSound(), SoundCategory.BLOCKS, 0.25f, 1.0f);
+                    if(!player.getBlockPos().equals(new BlockPos(pos.getX(), newY, pos.getZ()))){
+                        player.teleport(pos.getX() + 0.5, newY, pos.getZ() + 0.5);
+                        world.playSound(null, player.getBlockPos(), world.getBlockState(pos).getBlock().getSoundGroup(world.getBlockState(pos)).getStepSound(), SoundCategory.BLOCKS, 0.25f, 1.0f);
+                        return ActionResult.SUCCESS;
+                    }else{
+                        return ActionResult.PASS;
+                    }
+                }else{
+                    return ActionResult.PASS;
+                }
             }
         }
-
-        return ActionResult.SUCCESS;
+        return ActionResult.PASS;
     }
 }
